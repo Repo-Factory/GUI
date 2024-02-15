743b05c19ae9160c6fdd31b70158e32b6095c775
Dice Labs <dicelabs99@gmail.com>
2024-02-14 19:54:50 -0800

    Add Desktop Icon

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Husky GUI
 
 This package lays out a user interface for the husky robot to easily interact with the robots' different functionalities
+
+## Getting Started
+
+To get the gui on your computer run
+
+    git clone https://github.com/DiceLabs/gui
+
+If you want the icon to work, this must be clone to the $HOME/gui directory
+
+Then you can get icon by using the install script of the desk_icon
+
+    cd desk_icon
+    bash icon_install.sh
+    cd ..
---
+++ desk_icon/icon_install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+current_dir=$(pwd)
+DESKTOP_FILE="robot.desktop"
+DESKTOP_DIR="$HOME/Desktop"
+EXEC_FILE="$current_dir/launch_gui.sh"
+ICON_FILE="$current_dir/robot_icon.jpeg"
+app_name="GUI"
+
+cat > ${DESKTOP_FILE} << EOF
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=${app_name}
+Exec=${EXEC_FILE}
+Icon=${ICON_FILE}
+Terminal=true
+StartupNotify=true
+X-MultipleArgs=false
+X-Desktop-File-Install-Version=0.26
+EOF
+
+echo "Desktop entry file '${DESKTOP_FILE}' has been created/updated."
+
+cp $DESKTOP_FILE $DESKTOP_DIR
+chmod +x $DESKTOP_DIR/$DESKTOP_FILE
+
